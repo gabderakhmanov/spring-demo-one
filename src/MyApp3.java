985eeb9f90ed61c6862dcef14750939f29fd68f5
gabderakhmanov <ugla87@yandex.ru>
2017-05-27 21:44:00 +0200

    init and destroy methods in bean

---
+++ src/MyApp3.java
@@ -0,0 +1,19 @@
+import com.team.springdemo.teacher.ChemistryTeacher;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by maxim on 5/27/17.
+ */
+public class MyApp3 {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new
+                ClassPathXmlApplicationContext("beanLifecycle-applicationContext.xml");
+
+        ChemistryTeacher teacher = context.getBean(ChemistryTeacher.class);
+        teacher.teachChemistry();
+
+        context.close();
+
+    }
+}
---
+++ src/com/team/springdemo/teacher/ChemistryTeacher.java
@@ -0,0 +1,19 @@
+package com.team.springdemo.teacher;
+
+/**
+ * Created by maxim on 5/27/17.
+ */
+public class ChemistryTeacher {
+
+    public void teachChemistry(){
+        System.out.println("Teaching chemistry");
+    }
+
+    public void doInitStuff(){
+        System.out.println("Chemistry Teacher doing some INIT stuff");
+    }
+
+    public void doDestroyStuff(){
+        System.out.println("Chemistry Teacher doing some DESTROY stuff");
+    }
+}
